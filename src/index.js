03f64d1749a9daac786ba2a17afb9013afb47b7b
Shine32 <78789419+Shine32@users.noreply.github.com>
2021-02-09 14:28:14 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Welcome = () =>{
+	return(
+			<Text>Welcome to My Package Component.</Text>
+	)
+}
+
+export default Welcome;
